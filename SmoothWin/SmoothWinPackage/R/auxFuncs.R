72ff60780d27140f5c73eb824c894ee168c11441
hamedhm <hamedhm@ebi.ac.uk>
2018-11-29 11:44:39 +0000

    Upload the original codes

---
+++ SmoothWin/SmoothWinPackage/R/auxFuncs.R
@@ -0,0 +1,408 @@
+.onAttach <- function(lib, pkg) {
+  packageStartupMessage(
+    paste0(
+      '\n ========================================================================',
+      '\n ~> If you have any question about this package contact me by            ',
+      '\n ~> hamedhm@ebi.ac.uk                                                    ',
+      '\n ~> This project is supported by European Bioinformatic Institute (EBI)  ',
+      '\n ~> https://www.mousephenotype.org/                                      ',
+      '\n ========================================================================'
+    ),
+    domain   = NULL,
+    appendLF = TRUE
+  )
+}
+###########
+expF = function(x, k, l, m, cdf) {
+  m  = unique(m)
+  r = cdf(x,
+          m - l,
+          1 / k) *
+    (1 - (cdf(x,
+              m + l,
+              1 / k)))
+  r = r / ifelse(!is.null(r) &&
+                   max(r, na.rm = TRUE) != 0, max(r, na.rm = TRUE), 1)
+  return(r)
+}
+###########
+expWeight = function(t                         ,
+                     k                         ,
+                     l                         ,
+                     m                = 0      ,
+                     plot             = FALSE  ,
+                     zeroCompensation = 0      ,
+                     cdf              = plogis ,
+                     progress         = FALSE  ,
+                     ...) {
+  requireNamespace('Rfast')
+  m  = unique(m)
+  lm = length(m)
+  r  = matrix(0, ncol = lm, nrow = length(t))
+  if (progress)
+    pb = txtProgressBar(
+      min = 0  ,
+      max = lm,
+      style = 3,
+      width = 50,
+      char = '>'
+    )
+  ############################################################
+  r = sapply(X = 1:lm, function(i) {
+    SmoothWin:::expF(
+      x = t,
+      k = k,
+      l = l,
+      m = m[i],
+      cdf = cdf
+    )
+  })
+  ############################################################
+  # Begin. Just process the rows that are not zero! 
+  FinalS = rowSums(r)
+  r = r[FinalS != 0, , drop = FALSE]
+  ############################################################
+  s = 0
+  for (j in 1:lm) {
+    val = sapply(
+      X = combn(lm, j, simplify = FALSE),
+      FUN = function(i) {
+        val = (-1) ^ (j + 1)  * rowprods(r[, i, drop = FALSE])
+      }
+    )
+    s = s + rowSums(val)
+    if (progress)
+      setTxtProgressBar(pb, j)
+  }
+  ############################################################
+  # End. Just process the rows that are not zero! 
+  FinalS[FinalS != 0] = s
+  s = FinalS
+  ############################################################
+  if (progress) {
+    close(pb)
+    message('# Zero rows = ',
+            sum(FinalS == 0),
+            ' [',
+            round(sum(FinalS == 0) / length(FinalS) * 100,1),
+            '%]')
+  }
+  ############################################################
+  # Scale the weights to [0-1] on the data INTERVAL
+  # Note that the weights are always in [0-1] interval but
+  # not necessarily in the domain of the data
+  # Do not use the scale() to get only a plain vector of values
+  ############################################################
+  MaxMin = max(s, na.rm = TRUE) - min(s, na.rm = TRUE)
+  if (!is.nan(MaxMin) &&
+      !is.na(MaxMin) &&
+      MaxMin > sqrt(.Machine$double.eps)) {
+    s = (s - min(s, na.rm = TRUE)) / MaxMin
+  }
+  ############################################################
+  # For the problems that the machine epsilon needed
+  ############################################################
+  s[s <= zeroCompensation] =  zeroCompensation
+  ############################################################
+  # Plot output
+  ############################################################
+  if (plot) {
+    plot.weight(
+      weight = s      ,
+      t = t           ,
+      m = m           ,
+      l = l           ,
+      ...
+    )
+  }
+  return(s)
+}
+
+# Plot weights (only used in expWeight() function)
+plot.weight = function(weight, t, m, l, ...) {
+  m  = unique(m)
+  lm = length(m)
+  plot(
+    t,
+    weight,
+    xlab = 'Time',
+    ylab = 'Weight',
+    xaxt = 'n',
+    xlim = c(min(t, na.rm = TRUE) * .85, max(t, na.rm = TRUE) * 1.5),
+    ...
+  )
+  abline(
+    v   = c(m)  ,
+    lty = 1     ,
+    col = 1:lm  ,
+    lwd = 2
+  )
+  abline(v = c(m + l, m - l)   ,
+         lty = 2               ,
+         col = 1:lm)
+  axis(
+    side = 1                        ,
+    at =   c(m, m - l, m + l)       ,
+    labels = c(m, m - l, m + l)     ,
+    lwd.ticks = 2
+  )
+  legend(
+    'topright'                                                               ,
+    title = '[m, m+L, m-L]'                                                  ,
+    legend = paste(round(m, 3), round(m + l, 3), round(m - l, 3), sep = ', '),
+    col = 1:lm                                                               ,
+    fill = 1:lm
+  )
+}
+###########
+lseq = function (from = 1,
+                 to = 5,
+                 length.out = 6,
+                 adj = 1)
+{
+  r = exp(seq(log(from) / adj, log(to), length.out = length.out))
+  return(r)
+}
+###########
+msg = function(args, ...) {
+  message(
+    '\n Min observation required: ' ,
+    args$min.obs + length(args$m),
+    '\n  The number of modes: ',
+    length(unique(args$t[args$m])),
+    '\n  The time      range: ',
+    paste(round(range(args$t), 3), collapse = ' to '),
+    '\n  The bandwidth range: ',
+    paste(round(range(args$l), 3), collapse = ' to '),
+    ' [',
+    length(args$l),
+    ' split]',
+    '\n  The shape     range: ',
+    paste(round(range(args$k), 3), collapse = ' to '),
+    ' [',
+    length(args$k),
+    ' split]',
+    '\n  Threshold          : ',
+    args$threshold,
+    '\n',
+    if (sum(args$pvalThreshold) <= 0) {
+      paste0(
+        ' Sensitivity: ',
+        '\n\t mean = ',
+        args$sensitivity[1],
+        '\n\t variance = ',
+        args$sensitivity[2],
+        '\n\t mean*variance = ',
+        args$sensitivity[3]
+      )
+    } else{
+      paste0(
+        ' p-Value threshold: ',
+        '\n\t mean = ',
+        args$pvalThreshold[1],
+        '\n\t variance = ',
+        args$pvalThreshold[2],
+        '\n\t mean*variance = ',
+        args$pvalThreshold[3]
+      )
+    }
+  )
+}
+###########
+checkWeightsN <- function(w             ,
+                          t             ,
+                          check = 1     ,
+                          #0 off, #1 all+NoSingleDate, #2 (1) but SingleDates allowed
+                          threshold = 10 ^ -18,
+                          normalise = FALSE) {
+  n  = length(t)
+  if (!is.null(w) && check > 0) {
+    r      = 1:n
+    zw     = which(abs(w) >= threshold)
+    #### need at least 2 observations in a group
+    if (check == 1) {
+      t      = as.character(t)
+      tz     = table(t[zw])
+      MorTh1 = names(tz)[which(tz > 1)]
+      zw     = zw[t[zw] %in% MorTh1] # no singleDay-singleData
+    }
+    if (length(zw) > 0) {
+      r  = r[zw]
+      if (normalise) {
+        w  = w[zw] / sum(w[zw])
+      } else{
+        w  = w[zw]
+      }
+    } else{
+      message(
+        '\n ~> Windowing weights are ignored! (1) weights may be all close to zero. (2) there may be only one date in dataset [can cause errors in the mixed model]. (3) Setting check = 1 or check = 2 may solve the problem.\n'
+      )
+      w = (w * 0 + 1)
+      r = 1:n
+    }
+  } else{
+    r = 1:n
+  }
+  return(list(
+    w = w,
+    wInd = r,
+    NoZeWe = sum(w >= threshold)
+  ))
+}
+###########
+tv.test = function(obj                           ,
+                   args                          ,
+                   name          = 'parameter'   ,
+                   sensitivity   = c(1, 1, 1)    ,
+                   pvalThreshold = c(0, 0, 0)    ,
+                   residFun      = function(m) {
+                     resid(m)
+                   }                             ,
+                   predictFun    = function(m) {
+                     predict(m)
+                   },
+                   debug         = FALSE         ,
+                   ...) {
+  if (!is.null(obj$models) && length(obj$models) > 1) {
+    ldata  = nrow(obj$data)
+    dfv    = lapply(obj$models, residFun)
+    dfp    = lapply(obj$models, predictFun)
+    tt     = c()
+    for (i in 2:length(obj$models)) {
+      oi     = obj$weights[[i]]
+      oim    = obj$weights[[i - 1]]
+      thresh = 0 #args$threshold*0
+      # Variations
+      vtl  = var.test(dfv[[i]][oi >= thresh], dfv[[i - 1]][oim >= thresh])$p.value
+      vtlp = var.test(dfp[[i]][oi >= thresh], dfp[[i - 1]][oim >= thresh])$p.value
+      # Means
+      ttl  = t.test  (dfv[[i]][oi >= thresh], dfv[[i - 1]][oim >= thresh])$p.value
+      ttlp = t.test  (dfp[[i]][oi >= thresh], dfp[[i - 1]][oim >= thresh])$p.value
+      
+      if (sum(pvalThreshold) <= 0) {
+        tt[i - 1] =
+          sensitivity[3] * ((vtl * ttl) + (vtlp * ttl) + (vtl * ttlp) + (vtlp * ttlp) +
+                              (vtl * vtlp * ttl) + (vtl * vtlp * ttlp) +
+                              (vtl * ttl * ttlp) + (vtlp * ttl * ttlp) +
+                              (vtl * ttl * vtlp * ttlp)
+          ) +
+          sensitivity[2] * (vtl + vtlp + vtl * vtlp)  +
+          sensitivity[1] * (ttl + ttlp + ttl * ttlp)
+      } else{
+        f01 = function(x, thresh) {
+          (x > thresh) + (x <= thresh) * x
+        }
+        tt[i - 1] =
+          sensitivity[3] * (
+            f01(vtl * ttl     , pvalThreshold[3]) +
+              f01(vtlp * ttl  , pvalThreshold[3]) +
+              f01(vtl * ttlp  , pvalThreshold[3]) +
+              f01(vtlp * ttlp , pvalThreshold[3])
+          ) +
+          sensitivity[2] * (
+            f01(vtl , pvalThreshold[2]) + f01(vtlp , pvalThreshold[2]) +
+              f01(vtl , pvalThreshold[2]) * f01(vtlp , pvalThreshold[2])
+          ) +
+          sensitivity[1] * (
+            f01(ttl , pvalThreshold[1]) + f01(ttlp , pvalThreshold[1]) +
+              f01(ttl , pvalThreshold[1]) * f01(ttlp , pvalThreshold[1])
+          )
+      }
+    }
+    ####
+    al0 = data.frame(
+      tp.pval = c(tt, Inf),
+      ol      = obj$output$ObsInInterval,
+      l       = obj$output[, name]
+    )
+    mimi      = min(args$min.obs + length(args$m), ldata, na.rm = TRUE)[1]
+    pal       =     al0$ol >= ifelse(name == 'l', mimi, 0)
+    pmin      = min(al0$tp.pval[pal], Inf, na.rm = TRUE)
+    final     =    (al0$l[pal & (al0$tp.pval %in% pmin)])[1]
+    
+    if (is.null(final)      ||
+        is.null(pmin)       ||
+        length(final) < 1   ||
+        length(pmin)  < 1   ||
+        is.infinite(final)  ||
+        is.infinite(pmin)   ||
+        is.na(final)        ||
+        is.na(pmin)) {
+      message(paste('\n An optimal', name, 'is not found. Max value will be used.'))
+      final = NULL
+    } else{
+      message ('\n score: ',
+               round(pmin, digits = 4),
+               '. ',
+               name,
+               ': ',
+               round(final, 4))
+      # ##### Debug
+      if (debug) {
+        plot(
+          al0$l                               ,
+          al0$tp.pval                         ,
+          type = 'b'                          ,
+          ylab = 'Residual/Predict test pvals',
+          xlab = name,
+          main = name
+        )
+        abline(
+          v = final,
+          col = 2  ,
+          lty = 3  ,
+          lwd = 3
+        )
+        legend(
+          'bottomright' ,
+          fill = 2      ,
+          legend = paste0('Final ', name, ' = ', round(final, 4))
+        )
+        plot(
+          al0$l                              ,
+          al0$ol                             ,
+          type = 'b'                         ,
+          ylab = paste0(
+            'Sum(weight), MinSumRequiredIncludingTreatment = ' ,
+            round(mimi, 4)
+          ),
+          xlab = name                        ,
+          main = name
+        )
+        abline(
+          v = final                          ,
+          h = mimi                           ,
+          col = c(2, 3)                      ,
+          lty = c(3, 4)                      ,
+          lwd = c(1, 3)
+        )
+        legend('bottomright'                      ,
+               fill = 2:3,
+               legend = c(
+                 paste0('MinSumRequiredInclTr = ', round(mimi, 4)),
+                 paste0('final ', name, ' = ', round(final, 4))
+               ))
+      }
+      # #####
+    }
+  } else{
+    final = NULL
+  }
+  return(final)
+}
+###########
+is.function0 = function(FUN          ,
+                        na.rm = TRUE ,
+                        sort  = TRUE ,
+                        ...) {
+  if (is.function(FUN)) {
+    r = FUN()
+  } else{
+    r = FUN
+  }
+  if (na.rm)
+    r = r[!is.na(r)]
+  if (sort)
+    r = sort(r, ...)
+  return(r)
+}
---
+++ SmoothWin/SmoothWinPackage/R/main.R
@@ -0,0 +1,229 @@
+SmoothWin = function(object                                           ,
+                     data                                             ,
+                     t                                                ,
+                     m                                                ,
+                     l = function(ignore.me.in.default) {
+                       r = SmoothWin:::lseq(
+                         from = 1                                          ,
+                         to = max(abs(t[m] - min(t)), abs(t[m] - max(t)), 1),
+                         length.out = min(500, max(1, diff(range(
+                           t
+                         ))))
+                       )
+                       r = unique(round(r))
+                       return(r)
+                     },
+                     k = SmoothWin:::lseq(from = .5                   ,
+                                          to = 10                     ,
+                                          length.out = 50)            ,
+                     min.obs   = function(ignore.me.in.default) {
+                       lutm = length(unique(t[m]))
+                       r = ifelse(lutm > 1, 35, max(pi * sqrt(length(t)), 35))
+                       r = max(r * lutm, length(m), na.rm = TRUE)
+                       r = min(r       , length(t), na.rm = TRUE)
+                       return(r)
+                     }                                                ,
+                     weightFUN = function(x) {
+                       x
+                     }                                                ,
+                     residFun = function(x) {
+                       resid(x)
+                     }                                                ,
+                     predictFun = function(x) {
+                       predict(x)
+                     }                                                ,
+                     weightORthreshold = 'weight'                     ,
+                     cdf = plogis                                     ,
+                     check = 2                                        ,
+                     sensitivity   = c(1, 1, 1)                       ,
+                     pvalThreshold = c(0, 0, 0)                       ,
+                     threshold     = sqrt(.Machine$double.eps) * 10   ,
+                     zeroCompensation = 0                             ,
+                     messages      = TRUE                             ,
+                     seed          = NULL                             ,
+                     simple.output = FALSE                            ,
+                     debug         = FALSE                            ,
+                     ...) {
+  if (!is.null(seed))
+    set.seed(seed)
+  min.obs = ceiling(is.function0(min.obs))
+  l = is.function0(l, decreasing = FALSE)
+  k = is.function0(k, decreasing = TRUE)
+  argg    = c(as.list(environment()), list())
+  if (length(unique(t[m])) > 15)
+    message('More than 15 modes detected. The entire procedure can take a longer than usual!')
+  
+  if (length(m) > min.obs) {
+    stop('`min.obs` is less than the total number of treatments!')
+  } else {
+    msg(argg)
+  }
+  ### 1. Determining l
+  message('\n 1|3 Searching for l ...\n')
+  rl = gridSearchModel(
+    object = object                       ,
+    data = data                           ,
+    weightFUN = weightFUN                 ,
+    check = check                         ,
+    t = t                                 ,
+    m = t[m]                              ,
+    l = l                                 ,
+    k = max(k)                            ,
+    threshold = threshold                 ,
+    messages = messages                   ,
+    onlyOne  = FALSE                      ,
+    cdf      = cdf                        ,
+    zeroCompensation = zeroCompensation   ,
+    weightOrthreshold = weightORthreshold ,
+    ...
+  )
+  finall = tv.test(
+    obj = rl                              ,
+    args = argg                           ,
+    name = 'l'                            ,
+    residFun = residFun                   ,
+    sensitivity = sensitivity             ,
+    pvalThreshold = pvalThreshold         ,
+    predictFun = predictFun               ,
+    debug = debug
+  )
+  if (is.null(finall))
+    finall = max(l)
+  ### 2. Determining k
+  message('\n 2|3 Searching for k ...\n')
+  rk = gridSearchModel(
+    object = object                       ,
+    data = data                           ,
+    weightFUN = weightFUN                 ,
+    check = check                         ,
+    t = t                                 ,
+    m = t[m]                              ,
+    l = finall                            ,
+    k = k                                 ,
+    threshold = threshold                 ,
+    messages = messages                   ,
+    onlyOne  = FALSE                      ,
+    cdf      = cdf                        ,
+    zeroCompensation = zeroCompensation   ,
+    weightOrthreshold = weightORthreshold ,
+    ...
+  )
+  finalk = tv.test(
+    obj = rk                              ,
+    args = argg                           ,
+    name = 'k'                            ,
+    residFun = residFun                   ,
+    predictFun = predictFun               ,
+    sensitivity = sensitivity             ,
+    pvalThreshold = pvalThreshold         ,
+    debug = debug
+  )
+  if (is.null(finalk))
+    finalk = max(k)
+  
+  ##### final model
+  message('\n 3|3 Forming the final model ...\n')
+  finalr = gridSearchModel(
+    object = object                       ,
+    data = data                           ,
+    weightFUN = weightFUN                 ,
+    check = check                         ,
+    t = t                                 ,
+    m = t[m]                              ,
+    l = finall                            ,
+    k = finalk                            ,
+    threshold = threshold                 ,
+    messages = messages                   ,
+    onlyOne  = TRUE                       ,
+    cdf      = cdf                        ,
+    zeroCompensation = zeroCompensation   ,
+    weightOrthreshold = weightORthreshold ,
+    ...
+  )
+  if (simple.output) {
+    rk = rl = NULL
+  }
+  out = list(
+    object  = object                    ,
+    data    = data                      ,
+    final.k = finalk                    ,
+    final.l = finall                    ,
+    finalModel = finalr                 ,
+    model.l = rl                        ,
+    model.k = rk                        ,
+    min.obs = min.obs                   ,
+    input   = argg
+  )
+  class(out) = 'SmoothWin'
+  return(out)
+}
+
+
+
+# Plot windowing object
+plot.SmoothWin = function(x, ylab = 'Response', col = NULL ,   ...) {
+  if (!is.null(x$finalModel$models)) {
+    t  = x$input$t
+    y  = x$data[, all.vars(formula(x$finalModel$models))[1]]
+    m  = x$input$m
+    ly = length(y)
+    if (is.unsorted(t, na.rm = TRUE))
+      message('To get the right plot, make sure that the dataset is sorted on time!')
+    
+    if (is.null(col)) {
+      col = rgb(
+        abs(.8 - x$finalModel$FullWeight),
+        1 - x$finalModel$FullWeight      ,
+        1 - x$finalModel$FullWeight
+      )
+    }
+    plot(
+      t,
+      y,
+      xlab = 'Time',
+      ylab = ylab,
+      sub = paste(
+        'l='                                                      ,
+        round(x$final.l, 2)                                       ,
+        ', k='                                                    ,
+        round(x$final.k, 2)                                       ,
+        ', '                                                      ,
+        ifelse(x$input$weightORthreshold == 'weight', 'ASS=', '#'),
+        round(x$finalModel$output$ObsInInterval, 3)               ,
+        ' [~'                                                     ,
+        round(x$finalModel$output$ObsInInterval / ly * 100)       ,
+        '%]'       ,
+        ', MaxBW=' ,
+        max(x$input$l, na.rm = TRUE),
+        ', MinObs=',
+        x$min.obs  ,
+        '+'        ,
+        length(m)  ,
+        sep = ''
+      ),
+      col = col,
+      ...
+    )
+    
+    abline(v = unique(t[m]),
+           lty = 2,
+           col = 'gray')
+    wp = x$finalModel$FullWeight
+    lines(
+      t,
+      min(y) + wp * (max(y) - min(y)),
+      col = 'gray'                   ,
+      lty = 4                        ,
+      lwd = 4
+    )
+    return(invisible(list(
+      weight = wp   ,
+      k = x$final.k ,
+      l = x$final.k ,
+      object = x
+    )))
+  } else{
+    message('Windowing failed. No plot available for the failed models.')
+  }
+  #####
+}
