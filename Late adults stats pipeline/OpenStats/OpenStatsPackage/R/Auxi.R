714512a4f73a4be81db8bbdbfb6acae521712c5f
Hamed Haseli <hamedhaseli@gmail.com>
2020-02-14 14:23:07 +0000

    remove the bug with anova
    Removed the bug with the anova and added a function to remove zero data from Sex-Genotype contingency table

--- Late adults stats pipeline/OpenStats/OpenStatsPackage/R/Auxi.R
+++ Late adults stats pipeline/OpenStats/OpenStatsPackage/R/Auxi.R
@@ -2608,7 +2608,7 @@ modelSummaryPvalueExtract = function(x,
 		if (any(class(x) %in% 'glm')) {
 			mOrg = anova0(x, test = 'LRT')
 		} else{
-			mOrg = anova0(x)
+			mOrg = anova0(x, type = 'marginal')
 		}
 	} else{
 		mOrg  = coef(summary1(x))
@@ -3802,4 +3802,26 @@ isVariableCategorical = function(var = NULL, data = NULL) {
 		return(FALSE)
 	r = ifelse(is.factor(data[, var]), TRUE, FALSE)
 	return(r)
+}
+
+RemoveSexWithZeroDataPointInGenSexTableOnlyStatsPipelinenotExposed = function(object,
+																																							cols = c ('Genotype', 'Sex')) {
+	if (is.null(object) ||
+			nrow(object@datasetPL) < 1                          ||
+			!colExists(name = cols[1], data = object@datasetPL) ||
+			!colExists(name = cols[2], data = object@datasetPL))
+		return(object)
+	
+	tbl = table(object@datasetPL[, cols[1]], object@datasetPL[, cols[2]])
+	if (length(dim(tbl)) > 1) {
+		zc = as.data.frame(tbl)
+		zc = zc[zc$Freq < 1, ]
+		if (nrow(zc) > 0) {
+			message0('Zero frequency values detected ...')
+			names(zc)[1:length(cols)]   = cols
+			object@datasetPL = object@datasetPL[!object@datasetPL[, cols[-1]] %in% zc[, cols[-1]], ]
+			object@datasetPL = droplevels(object@datasetPL)
+		}
+	}
+	return(object)
 }
