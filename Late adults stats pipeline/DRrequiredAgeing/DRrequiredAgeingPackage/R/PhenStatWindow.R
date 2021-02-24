71792416fe8eb67f9dde294a9e4d2c370209f93d
Hamed Haseli Mashhadi <hamedhm@ebi.ac.uk>
2021-02-24 16:12:36 +0000

    temp commit for the ABR data- make sure this is taken back!

--- Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/R/PhenStatWindow.R
+++ Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/R/PhenStatWindow.R
@@ -79,7 +79,7 @@ PhenStatWindow = function (phenlistObject                                ,
       as.formula('~ 1 | Batch')
     else
       as.formula('~ 1 | Batch')
-    CorrEffect  = NULL
+    CorrEffect  = nlme::corSymm()
   }
   ###########################
   # Run normal (not windowed) models
