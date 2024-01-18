36847f531c57770f8ee75c38a0fb1ae443b81d0e
Marina Kan <marinak@ebi.ac.uk>
2024-01-18 10:04:54 +0000

    Fix logs in Step3MergeRdataFilesJobs.R

--- Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/inst/extdata/StatsPipeline/0-ETL/Step3MergeRdataFilesJobs.R
+++ Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/inst/extdata/StatsPipeline/0-ETL/Step3MergeRdataFilesJobs.R
@@ -1,19 +1,17 @@
-f = function(path = file.path(getwd(), 'ProcedureScatterRdata'),
-             mem = 80000) {
-  dirs = list.dirs(path = path,
-                   full.names = TRUE ,
-                   recursive  = FALSE)
-  ############## jobs creator#
+############# jobs creator for MergeRdatas
+f <- function(path = file.path(getwd(), "ProcedureScatterRdata"), mem = 80000) {
+  dirs <- list.dirs(path = path,
+                    full.names = TRUE,
+                    recursive  = FALSE)
+  unique_dirs <- unique(na.omit(dirs))
   write(
     paste0(
-      'bsub -q bigmem -J IMPC_stats_pipeline_lsf_jobs -M ',
-      mem,
-      ' -e "step4_MergeRdatas_error.log" -o "step4_MergeRdatas_output.log" Rscript Step4MergingRdataFiles.R "',
-      unique(na.omit(dirs)),
-      '"'
+      "bsub -q bigmem -J IMPC_stats_pipeline_lsf_jobs -M ", mem,
+      " -e ", unique_dirs, "/step4_merge_rdatas.err ",
+      "-o ", unique_dirs, "/step4_merge_rdatas.log ",
+      "Rscript Step4MergingRdataFiles.R ", unique_dirs
     ),
-    file = 'jobs_step4_MergeRdatas.bch'
+    file = "jobs_step4_MergeRdatas.bch"
   )
-  write('',file = 'Step3 completed.log')
+  write("", file = "step3_completed.log")
 }
-
