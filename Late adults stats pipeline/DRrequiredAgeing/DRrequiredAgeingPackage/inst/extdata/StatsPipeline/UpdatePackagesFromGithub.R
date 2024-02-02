cad7776b72938d2118f79c05dc8352b87d4143d6
Marina Kan <marinak@ebi.ac.uk>
2024-02-02 14:12:39 +0000

    Edit install_github to download from dev branch

--- Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/inst/extdata/StatsPipeline/UpdatePackagesFromGithub.R
+++ Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/inst/extdata/StatsPipeline/UpdatePackagesFromGithub.R
@@ -1,40 +1,31 @@
 library(devtools)
-# DRrequired
-install_github(
-  repo = 'mpi2/impc_stats_pipeline/Early adults stats pipeline/DRrequired/DRrequiredPackage',
-  dependencies = FALSE,
-  upgrade = 'always',
-  force = TRUE,
-  build = TRUE,
-  quiet = FALSE
-)
-
 # DRrequiredAgeing
 install_github(
-  repo = 'mpi2/impc_stats_pipeline/Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage',
+  repo = "mpi2/impc_stats_pipeline/Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage",
   dependencies = FALSE,
-  upgrade = 'always',
+  upgrade = "always",
   force = TRUE,
   build = TRUE,
-  quiet = FALSE
+  quiet = FALSE,
+  ref = "dev"
 )
 
 # SmoothWin
 install_github(
-  repo = 'mpi2/impc_stats_pipeline/SoftWindowing/SmoothWin/SmoothWinPackage/',
+  repo = "mpi2/impc_stats_pipeline/SoftWindowing/SmoothWin/SmoothWinPackage/",
   dependencies = FALSE,
-  upgrade = 'always',
+  upgrade = "always",
   force = TRUE,
   build = TRUE,
-  quiet = FALSE
+  quiet = FALSE,
+  ref = "dev"
 )
 
 # OpenStats
 install_github(
-  #repo = 'mpi2/impc_stats_pipeline/Late adults stats pipeline/OpenStats/OpenStatsPackage/',
-  repo = 'mpi2/OpenStats',
+  repo = "mpi2/OpenStats",
   dependencies = FALSE,
-  upgrade = 'always',
+  upgrade = "always",
   force = TRUE,
   build = TRUE,
   quiet = FALSE
