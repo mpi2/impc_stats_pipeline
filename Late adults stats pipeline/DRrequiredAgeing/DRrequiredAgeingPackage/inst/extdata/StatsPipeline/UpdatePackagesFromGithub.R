e786abd377864d7ba4cc52987c43024e5afd0073
Marina Kan <marinak@ebi.ac.uk>
2024-02-23 10:48:08 +0000

    Add two arguments, repository name and branch name to the UpdatePackagesFromGithub.R

--- Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/inst/extdata/StatsPipeline/UpdatePackagesFromGithub.R
+++ Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/inst/extdata/StatsPipeline/UpdatePackagesFromGithub.R
@@ -1,4 +1,14 @@
 library(devtools)
+
+# Check if the correct number of arguments is provided
+if (length(commandArgs(trailingOnly = TRUE)) != 2) {
+  stop("Please provide exactly two arguments: repository name and branch name.")
+}
+
+# Retrieve the command line arguments
+repository <- commandArgs(trailingOnly = TRUE)[1]
+branch <- commandArgs(trailingOnly = TRUE)[2]
+
 # DRrequiredAgeing
 install_github(
   repo = "marinak-ebi/impc_stats_pipeline/Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage",
