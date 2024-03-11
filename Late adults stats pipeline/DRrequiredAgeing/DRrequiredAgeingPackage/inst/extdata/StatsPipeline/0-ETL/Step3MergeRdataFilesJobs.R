2937bc6235215776c8bada5edc476e9a4c4d40e4
Marina Kan <marinak@ebi.ac.uk>
2024-03-11 11:17:10 +0000

    Replace bsub with sbatch in Step3MergeRdataFilesJobs.R

--- Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/inst/extdata/StatsPipeline/0-ETL/Step3MergeRdataFilesJobs.R
+++ Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/inst/extdata/StatsPipeline/0-ETL/Step3MergeRdataFilesJobs.R
@@ -1,12 +1,15 @@
 # Jobs creator for MergeRdatas
-f <- function(path = file.path(getwd(), "ProcedureScatterRdata"), mem = 80000) {
+f <- function(path = file.path(getwd(), "ProcedureScatterRdata"),
+              mem = "30G",
+              time = "00:55:00") {
   dirs <- list.dirs(path = path,
                     full.names = TRUE,
                     recursive  = FALSE)
   unique_dirs <- unique(na.omit(dirs))
   write(
     paste0(
-      "bsub -q bigmem -J impc_stats_pipeline_job -M ", mem,
+      "sbatch --job-name=impc_stats_pipeline_job --mem=", mem,
+      " --time=", time,
       " -e ", unique_dirs, "/step4_merge_rdatas.err",
       " -o ", unique_dirs, "/step4_merge_rdatas.log ",
       "Rscript Step4MergingRdataFiles.R ", unique_dirs
