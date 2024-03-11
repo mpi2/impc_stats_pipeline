1678d2128b6f44485711170524bf44863d629ecb
Marina Kan <marinak@ebi.ac.uk>
2024-03-11 12:00:44 +0000

    Remove copy of Step2Parquet2Rdata.R function ETLStep2Parquet2Rdata

--- Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/R/sideFunctions.R
+++ Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/R/sideFunctions.R
@@ -4559,55 +4559,6 @@ DeleteDirectoryAndSubDirectories = function(path  = getwd(),
   )
 }
 #### Only for the ETL process step 1,2,3,4
-ETLStep2Parquet2Rdata = function(files) {
-  requireNamespace(miniparquet)
-  df = lapply(seq_along(files),
-              function(i) {
-                message(i, '|', length(files), ' ~> ', files[i])
-                r = miniparquet::parquet_read(files[i])
-                return(r)
-              })
-  ###############
-  procedure_list = na.omit(unique(unlist(lapply(df, function(x) {
-    unique(x$procedure_group)
-  }))))
-
-  for (proc in procedure_list) {
-    message(which(procedure_list == proc),
-            '/',
-            length(procedure_list),
-            ' Checking for ',
-            proc)
-    rdata = data.table::rbindlist(lapply(df, function(x) {
-      r = subset(x, x$procedure_group == proc)
-      return(r)
-    }))
-
-    if (!is.null(rdata) &&
-        nrow    (rdata) > 0) {
-      rdata = rdata[!duplicated(rdata),]
-    }
-
-    outDir = file.path('ProcedureScatterRdata', proc)
-    if (!dir.exists(outDir))
-      dir.create(outDir, recursive = TRUE)
-    save(rdata,
-         file = file.path(
-           outDir,
-           paste(
-             round(runif(1) * 10 ^ 6)  ,
-             proc                  ,
-             '.Rdata'              ,
-             sep = '_'             ,
-             collapse = '_'
-           )
-         ),
-         compress = FALSE)
-    rm(rdata)
-    gc()
-  }
-}
-
 ETLStep3MergeRdataFilesJobs = function(path = file.path(getwd(), 'ProcedureScatterRdata'),
                                        mem = 80000) {
   dirs = list.dirs(path = path,
