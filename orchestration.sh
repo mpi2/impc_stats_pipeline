909bcaa4b62af48c9d56e883c5329f988d30a632
Marina Kan <marinak@ebi.ac.uk>
2024-08-21 16:24:23 +0100

    Create working directory

--- orchestration.sh
+++ orchestration.sh
@@ -4,3 +4,13 @@
 function message0() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') $1"
 }
+
+# Statistical pipeline
+message0 "Starting the IMPC statistical pipeline..."
+mkdir SP
+export input_path=$(realpath .)
+export sp_results=$(realpath SP)
+message0 "Parquet files path: ${input_path}"
+message0 "Output path: ${sp_results}"
+
+cd SP
