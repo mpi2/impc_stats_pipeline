87f530363ad415221e136d77d95ea569d08542d6
Marina Kan <marinak@ebi.ac.uk>
2025-02-28 10:27:30 +0000

    Move file processing to process_statpacket

--- annotation_pipeline/loader.py
+++ annotation_pipeline/loader.py
@@ -13,6 +13,50 @@ pandas2ri.activate()
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+def process_statpacket(file, output_file, mp_chooser_file):
+    data_table = importr("data.table")
+    df = data_table.fread(
+        file=file,
+        header=False,
+        sep="\t",
+        quote="",
+        stringsAsFactors=False
+    )
+
+    # Convert R's ncol and nrow to Python integers.
+    num_cols = int(r["ncol"](df)[0])
+    num_rows = int(r["nrow"](df)[0])
+
+    if num_cols != 20 or num_rows > 1:
+        logging.error("Invalid file format !=20 columns or >1 rows: %s", file)
+        return
+
+    # Call R's annotationChooser.
+    dr_required_ageing = importr("DRrequiredAgeing")
+
+    rN = dr_required_ageing.annotationChooser(
+        statpacket=df,
+        level=0.0001,
+        rrlevel=0.0001,
+        mp_chooser_file=mp_chooser_file
+    )
+
+    rW = dr_required_ageing.annotationChooser(
+        statpacket=rN.rx2("statpacket"),
+        level=0.0001,
+        rrlevel=0.0001,
+        resultKey="Windowed result",
+        TermKey="WMPTERM",
+        mp_chooser_file=mp_chooser_file
+    )
+
+    statpacket_v20_values = rW.rx2("statpacket").rx2("V20")
+
+    with open(output_file, "a", encoding="utf-8") as outfile:
+        outfile.write(
+            "".join(r["as.character"](statpacket_v20_values)) + "\n"
+        )
+
 def main():
     """Process statpackets using annotationChooser."""
     parser = argparse.ArgumentParser(
@@ -30,7 +74,7 @@ def main():
     logging.info("Starting loader.R with arguments:")
     logging.info("  file: %s", args.file)
     logging.info("  mp_chooser_file: %s", args.mp_chooser_file)
-    
+
     file_list_path = Path(args.file)
     mp_chooser_file = args.mp_chooser_file
 
@@ -38,7 +82,6 @@ def main():
     importr("base")
     importr("jsonlite")
     importr("utils")
-    data_table = importr("data.table")
 
     r["load"](mp_chooser_file)
 
@@ -59,49 +102,8 @@ def main():
             "NotProcessed" in file or "Successful" in file
         ):
             try:
-                df = data_table.fread(
-                    file=str(file_path),
-                    header=False,
-                    sep="\t",
-                    quote="",
-                    stringsAsFactors=False
-                )
-
-                # Convert R's ncol and nrow to Python integers.
-                num_cols = int(r["ncol"](df)[0])
-                num_rows = int(r["nrow"](df)[0])
-
-                if num_cols != 20 or num_rows > 1:
-                    logging.error("Invalid file format !=20 columns or >1 rows: %s", file)
-                    continue
-
-                # Call R's annotationChooser.
-                dr_required_ageing = importr("DRrequiredAgeing")
-
-                rN = dr_required_ageing.annotationChooser(
-                    statpacket=df,
-                    level=0.0001,
-                    rrlevel=0.0001,
-                    mp_chooser_file=mp_chooser_file
-                )
-
-                rW = dr_required_ageing.annotationChooser(
-                    statpacket=rN.rx2("statpacket"),
-                    level=0.0001,
-                    rrlevel=0.0001,
-                    resultKey="Windowed result",
-                    TermKey="WMPTERM",
-                    mp_chooser_file=mp_chooser_file
-                )
-
-                statpacket_v20_values = rW.rx2("statpacket").rx2("V20")
-
-                with open(output_file, "a", encoding="utf-8") as outfile:
-                    outfile.write(
-                        "".join(r["as.character"](statpacket_v20_values)) + "\n"
-                    )
+                process_statpacket(file, output_file, mp_chooser_file)
                 logging.info("File processed successfully %s out of %s: %s", i+1, total_files, file)
-
             except Exception as e:
                 logging.error(f"Error processing {file}:\n%s", e)
 
