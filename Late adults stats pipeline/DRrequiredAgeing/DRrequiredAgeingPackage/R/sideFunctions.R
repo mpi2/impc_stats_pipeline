588605d7cb7ba2a0bf1fa65f25718729a1f2a092
Marina Kan <marinak@ebi.ac.uk>
2025-06-03 16:33:29 +0100

    Remove detach_package function that never used

--- Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/R/sideFunctions.R
+++ Late adults stats pipeline/DRrequiredAgeing/DRrequiredAgeingPackage/R/sideFunctions.R
@@ -1583,22 +1583,6 @@ plot_win = function(phenlistObject, r, depVariable, check, ...) {
   }
 }
 
-
-detach_package <- function(pkg, character.only = FALSE)
-{
-  if (!character.only)
-  {
-    pkg <- deparse(substitute(pkg))
-  }
-  search_item <- paste("package", pkg, sep = ":")
-  while (search_item %in% search())
-  {
-    detach(search_item,
-           unload = TRUE,
-           character.only = TRUE)
-  }
-}
-
 # boxplot for window
 boxplot_win = function(phenlistObject, we, threshold, ...) {
   boxplot(
